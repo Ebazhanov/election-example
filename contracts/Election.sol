907ca196db9bc0e5ba5be87a599870d28304a893
Evgenii Bazhanov <evgenii.bazhanov@smava.de>
2019-02-03 18:21:50 +0100

    daily update

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,6 +1,13 @@
 pragma solidity ^0.5.0;
 
 contract Election{
+
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
         // Store candidate
         // Read candidate
     string public candidate;
--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.5.0;
 
 contract Migrations {
     address public owner;
-    uint public last_completed_migration;
+    uint public last_ompleted_migration;
 
     modifier restricted () {
         if (msg.sender == owner) _;
